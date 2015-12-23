a3cf3c227ab76ad6db8ef7b5ea167cceaae03878
norlin <alexey@norlin.guru>
2015-12-23 07:51:30 +0300

    Initial Commit

---
+++ mahanenko.ruTests/mahanenko_ruTests.swift
@@ -0,0 +1,36 @@
+//
+//  mahanenko_ruTests.swift
+//  mahanenko.ruTests
+//
+//  Created by norlin on 23/12/15.
+//  Copyright © 2015 norlin. All rights reserved.
+//
+
+import XCTest
+@testable import mahanenko_ru
+
+class mahanenko_ruTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
